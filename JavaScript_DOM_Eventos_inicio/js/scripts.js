3fd001a8c4b3e15340b506cb2202a229c33d5232
patxi <patxiarana05@gmail.com>
2023-09-08 13:04:56 -0300

    practices

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -16,9 +16,79 @@ enlaces[0].classList.add('nueva-clase');
 // Generar nuevo enlace 
 
 const nuevoEnlace = document.createElement('A')
+// Agregar al nuevo enlace
+nuevoEnlace.href='nuevo-enlace.html'; 
 
-nuevoEnlace.href='nuevo-enlace.html'
+//Agregar el texto al nuevo enlace
 
-<a href="nosotros.html" class="nuevo-enlace">Nosotros</a>
+nuevoEnlace.textContent = 'Tienda virtual'; 
+// Agregar a la clase
 
+nuevoEnlace.classList.add('navegacion__enlace')
+
+// Agregar al documento 
+
+const navegacion = document.querySelector('.navegacion')
+navegacion.appendChild(nuevoEnlace)
  console.log(nuevoEnlace)
+
+
+
+
+ // Eventos 
+/*
+
+console.log(1)
+window.addEventListener('load',function(){ //load espera a que el JS y los archivos que dependen del del HTML esten listo 
+    console.log(2)
+})
+
+window.onload = function() {
+    console.log(3)
+}
+
+
+document.addEventListener('DOMContentLoaded',function(){ // Solo espera por el HTML, pero no espera css o imagenes 
+    console.log(4)
+})
+
+console.log(5) */
+
+/*window.onscroll = function () {
+    console.log('scrolling...')
+} */
+
+
+
+// Seleccionar Elementos y asociarles un evento 
+
+const btnEnviar = document.querySelector('.boton--primario')
+btnEnviar.addEventListener('click', function(evento){
+console.log(evento)
+console.log(evento.preventDefault())
+    console.log('enviando formulario')
+})
+
+// Eventos de los inputs text area 
+
+
+const datos = {
+    nombre: '',
+    email: '', 
+    mensaje: '',
+
+}
+
+const nombre = document.querySelector('#nombre')
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+
+nombre.addEventListener('input', leerTexto)
+email.addEventListener('input', leerTexto)
+mensaje.addEventListener('input',leerTexto)
+
+function leerTexto(e) {
+
+    datos[e.target.id] = e.target.value ;
+    console.log(datos)
+}
